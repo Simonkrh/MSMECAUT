d339cec68424fd431d72b76d440dec4528f56618
Simonkho <simonkrh@gmail.com>
2025-10-24 14:03:48 +0200

    Assignment 3 - AIS4104 (T.2)

--- AIS4104/AIS4104-assignment 3/introduction/main.cpp
+++ AIS4104/AIS4104-assignment 3/introduction/main.cpp
@@ -3,34 +3,7 @@
 
 static constexpr double deg_to_rad = EIGEN_PI / 180.0;
 
-// ====================================== T.1 a) ======================================
-Eigen::VectorXd std_vector_to_eigen(const std::vector<double> &v)
-{
-    Eigen::VectorXd eigen_vector(v.size());
-    for (size_t i = 0; i < v.size(); ++i)
-    {
-        eigen_vector(i) = v[i];
-    }
-    return eigen_vector;
-}
-
-// ====================================== T.1 b) ======================================
-bool is_average_below_eps(const std::vector<double> &values, double eps = 10e-7, uint8_t n_values = 5u)
-{
-    if (values.size() < n_values)
-        return false;
-
-    double sum = 0.0;
-    for (size_t i = values.size() - n_values; i < values.size(); ++i)
-    {
-        sum += values[i];
-    }
-
-    double average = sum / n_values;
-    return average <= eps;
-}
-
-// ====================================== T.1 c) ======================================
+// ====================================== From previous assignments ======================================
 // Equation (3.62) page 87, MR pre-print 2019
 // From Assignment 1
 Eigen::Matrix4d transformation_matrix(const Eigen::Matrix3d &r, const Eigen::Vector3d &p)
@@ -77,41 +50,6 @@ static inline Eigen::Matrix4d A(double a, double alpha, double d, double theta)
     return T;
 }
 
-std::pair<Eigen::Matrix4d, std::vector<Eigen::VectorXd>> ur3e_space_chain()
-{
-    static const double a_link[6] = {0.0, -0.24355, -0.21320, 0.0, 0.0, 0.0};
-    static const double d_link[6] = {0.15185, 0.0, 0.0, 0.13105, 0.08535, 0.09210};
-    static const double alpha_link[6] = {EIGEN_PI / 2, 0.0, 0.0, EIGEN_PI / 2, -EIGEN_PI / 2, 0.0};
-
-    Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
-    Eigen::Vector3d o = Eigen::Vector3d::Zero();
-    Eigen::Vector3d z = Eigen::Vector3d::UnitZ();
-    Eigen::Matrix<double, 6, 1> S;
-
-    std::vector<Eigen::VectorXd> S_list;
-    S_list.reserve(6);
-
-    // Definition 3.24 on page 102v, MR pre-print 2019
-    for (int i = 0; i < 6; ++i)
-    {
-        S.head<3>() = z;
-        S.tail<3>() = -z.cross(o);
-
-        Eigen::VectorXd Si_vec(6);
-        Si_vec = S;
-        S_list.push_back(Si_vec);
-
-        T = T * A(a_link[i], alpha_link[i], d_link[i], 0.0);
-        o = T.block<3, 1>(0, 3);
-        z = T.block<3, 3>(0, 0) * Eigen::Vector3d::UnitZ();
-    }
-
-    // Equation (4.6) on page 136, MR pre-print 2019
-    // Home configuration M equals the T because M is the end-effector pose when all joint variables are 0.
-    return {T, S_list};
-}
-
-// ====================================== T.1 d) ======================================
 // Equation (3.30) page 75, MR pre-print 2019
 // From Assignment 1
 Eigen::Matrix3d skew_symmetric(Eigen::Vector3d vec)
@@ -144,25 +82,6 @@ Eigen::Matrix4d matrix_exponential(const Eigen::Vector3d &w, const Eigen::Vector
     return T;
 }
 
-Eigen::Matrix4d ur3e_space_fk(const Eigen::VectorXd &joint_positions)
-{
-    auto [M, S_list] = ur3e_space_chain();
-    Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
-
-    // Equation (4.14) on page 140, MR pre-print 2019
-    for (int i = 0; i < 6; ++i)
-    {
-        const Eigen::Vector3d w = S_list[i].head<3>();
-        const Eigen::Vector3d v = S_list[i].tail<3>();
-        const double theta = joint_positions[i];
-        T *= matrix_exponential(w, v, theta);
-    }
-
-    // Equation (4.6) on page 136, MR pre-print 2019
-    return T * M;
-}
-
-// ====================================== T.1 e) ======================================
 // Definition (3.20) on page 98, MR pre-print 2019
 // From assignment 2
 Eigen::MatrixXd adjoint_matrix(const Eigen::Matrix4d &T)
@@ -178,46 +97,6 @@ Eigen::MatrixXd adjoint_matrix(const Eigen::Matrix4d &T)
     return Ad;
 }
 
-// Equation (4.16) on page 147, MR pre-print 2019
-std::pair<Eigen::Matrix4d, std::vector<Eigen::VectorXd>> ur3e_body_chain()
-{
-    auto [M, S_list] = ur3e_space_chain();
-
-    Eigen::Matrix3d R = M.block<3, 3>(0, 0);
-    Eigen::Vector3d p = M.block<3, 1>(0, 3);
-    Eigen::Matrix4d M_inv = Eigen::Matrix4d::Identity();
-    M_inv.block<3, 3>(0, 0) = R.transpose();
-    M_inv.block<3, 1>(0, 3) = -R.transpose() * p;
-
-    Eigen::MatrixXd Ad_Minv = adjoint_matrix(M_inv);
-
-    // Equation (4.16) on page 147, MR pre-print 2019<
-    std::vector<Eigen::VectorXd> B_list(6);
-    for (int i = 0; i < 6; ++i)
-    {
-        B_list[i] = Ad_Minv * S_list[i];
-    }
-
-    return {M, B_list};
-}
-
-// ====================================== T.1 f) ======================================
-Eigen::Matrix4d ur3e_body_fk(const Eigen::VectorXd &joint_positions)
-{
-    auto [M, B_list] = ur3e_body_chain();
-
-    Eigen::Matrix4d T = M;
-    for (int i = 0; i < 6; ++i)
-    {
-        const Eigen::Vector3d w = B_list[i].head<3>();
-        const Eigen::Vector3d v = B_list[i].tail<3>();
-        const double theta = joint_positions[i];
-        T *= matrix_exponential(w, v, theta);
-    }
-    return T;
-}
-
-// ====================================== T.1 g) ======================================
 // Equations (B.3)–(B.5) on page 579, MR pre-print 2019
 // From assignment 2
 Eigen::Vector3d euler_zyx_from_rotation_matrix(const Eigen::Matrix3d &R)
@@ -262,6 +141,129 @@ void print_pose(const std::string &label, const Eigen::Matrix4d &T)
               << "Position:        " << p.transpose() << "\n\n";
 }
 
+// ====================================== T.1 a) ======================================
+Eigen::VectorXd std_vector_to_eigen(const std::vector<double> &v)
+{
+    Eigen::VectorXd eigen_vector(v.size());
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        eigen_vector(i) = v[i];
+    }
+    return eigen_vector;
+}
+
+// ====================================== T.1 b) ======================================
+bool is_average_below_eps(const std::vector<double> &values, double eps = 10e-7, uint8_t n_values = 5u)
+{
+    if (values.size() < n_values)
+        return false;
+
+    double sum = 0.0;
+    for (size_t i = values.size() - n_values; i < values.size(); ++i)
+    {
+        sum += values[i];
+    }
+
+    double average = sum / n_values;
+    return average <= eps;
+}
+
+// ====================================== T.1 c) ======================================
+std::pair<Eigen::Matrix4d, std::vector<Eigen::VectorXd>> ur3e_space_chain()
+{
+    static const double a_link[6] = {0.0, -0.24355, -0.21320, 0.0, 0.0, 0.0};
+    static const double d_link[6] = {0.15185, 0.0, 0.0, 0.13105, 0.08535, 0.09210};
+    static const double alpha_link[6] = {EIGEN_PI / 2, 0.0, 0.0, EIGEN_PI / 2, -EIGEN_PI / 2, 0.0};
+
+    Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
+    Eigen::Vector3d o = Eigen::Vector3d::Zero();
+    Eigen::Vector3d z = Eigen::Vector3d::UnitZ();
+    Eigen::Matrix<double, 6, 1> S;
+
+    std::vector<Eigen::VectorXd> S_list;
+    S_list.reserve(6);
+
+    // Definition 3.24 on page 102v, MR pre-print 2019
+    for (int i = 0; i < 6; ++i)
+    {
+        S.head<3>() = z;
+        S.tail<3>() = -z.cross(o);
+
+        Eigen::VectorXd Si_vec(6);
+        Si_vec = S;
+        S_list.push_back(Si_vec);
+
+        T = T * A(a_link[i], alpha_link[i], d_link[i], 0.0);
+        o = T.block<3, 1>(0, 3);
+        z = T.block<3, 3>(0, 0) * Eigen::Vector3d::UnitZ();
+    }
+
+    // Equation (4.6) on page 136, MR pre-print 2019
+    // Home configuration M equals the T because M is the end-effector pose when all joint variables are 0.
+    return {T, S_list};
+}
+
+// ====================================== T.1 d) ======================================
+Eigen::Matrix4d ur3e_space_fk(const Eigen::VectorXd &joint_positions)
+{
+    auto [M, S_list] = ur3e_space_chain();
+    Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
+
+    // Equation (4.14) on page 140, MR pre-print 2019
+    for (int i = 0; i < 6; ++i)
+    {
+        const Eigen::Vector3d w = S_list[i].head<3>();
+        const Eigen::Vector3d v = S_list[i].tail<3>();
+        const double theta = joint_positions[i];
+        T *= matrix_exponential(w, v, theta);
+    }
+
+    // Equation (4.6) on page 136, MR pre-print 2019
+    return T * M;
+}
+
+// ====================================== T.1 e) ======================================
+// Equation (4.16) on page 147, MR pre-print 2019
+std::pair<Eigen::Matrix4d, std::vector<Eigen::VectorXd>> ur3e_body_chain()
+{
+    auto [M, S_list] = ur3e_space_chain();
+
+    Eigen::Matrix3d R = M.block<3, 3>(0, 0);
+    Eigen::Vector3d p = M.block<3, 1>(0, 3);
+    Eigen::Matrix4d M_inv = Eigen::Matrix4d::Identity();
+    M_inv.block<3, 3>(0, 0) = R.transpose();
+    M_inv.block<3, 1>(0, 3) = -R.transpose() * p;
+
+    Eigen::MatrixXd Ad_Minv = adjoint_matrix(M_inv);
+
+    // Equation (4.16) on page 147, MR pre-print 2019<
+    std::vector<Eigen::VectorXd> B_list(6);
+    for (int i = 0; i < 6; ++i)
+    {
+        B_list[i] = Ad_Minv * S_list[i];
+    }
+
+    return {M, B_list};
+}
+
+// ====================================== T.1 f) ======================================
+// Equation (4.16) on page 147, MR pre-print 2019
+Eigen::Matrix4d ur3e_body_fk(const Eigen::VectorXd &joint_positions)
+{
+    auto [M, B_list] = ur3e_body_chain();
+
+    Eigen::Matrix4d T = M;
+    for (int i = 0; i < 6; ++i)
+    {
+        const Eigen::Vector3d w = B_list[i].head<3>();
+        const Eigen::Vector3d v = B_list[i].tail<3>();
+        const double theta = joint_positions[i];
+        T *= matrix_exponential(w, v, theta);
+    }
+    return T;
+}
+
+// ====================================== T.1 g) ======================================
 void ur3e_test_fk()
 {
     std::cout << "Forward kinematics tests" << std::endl;
@@ -290,7 +292,98 @@ void ur3e_test_fk()
                ur3e_body_fk(std_vector_to_eigen({0.0, 0.0, -90.0, 0.0, 0.0, 0.0}) * deg_to_rad));
 }
 
+// ====================================== T.2 a) ======================================
+// Section (6.2.1) on page 225, MR pre-print 2019
+std::pair<uint32_t, double> newton_raphson_root_find(
+    const std::function<double(double)> &f,
+    double x_0,
+    double dx_0 = 0.5,
+    double eps = 10e-7)
+{
+    const uint32_t max_iterations = 1000;
+    double x = x_0;
+
+    for (uint32_t i = 0; i < max_iterations; ++i)
+    {
+        double fx = f(x);
+
+        if (std::abs(fx) < eps)
+            return {i, x};
+
+        // Approximate derivative using central difference
+        // Equation taken from https://en.wikipedia.org/wiki/Finite_difference
+        double dfdx = (f(x + dx_0) - f(x - dx_0)) / (2.0 * dx_0);
+
+        // Newton–Raphson update
+        double x_new = x - fx / dfdx;
+
+        if (std::abs(x_new - x) < eps)
+            return {i + 1, x_new};
+
+        x = x_new;
+    }
+
+    return {max_iterations, x};
+}
+
+// ====================================== T.2 b) ======================================
+// Gradient Descent equation taken from https://en.wikipedia.org/wiki/Gradient_descent
+std::pair<uint32_t, double> gradient_descent_minimize(
+    const std::function<double(double)> &f,
+    double x_0,
+    double gamma = 0.1, // η
+    double dx_0 = 0.5,
+    double eps = 10e-7)
+{
+    const uint32_t max_iterations = 1000;
+    double x = x_0;
+
+    for (uint32_t i = 0; i < max_iterations; ++i)
+    {
+        // Approximate derivative using central difference
+        // Equation taken from https://en.wikipedia.org/wiki/Finite_difference
+        const double grad = (f(x + dx_0) - f(x - dx_0)) / (2.0 * dx_0);
+
+        if (std::abs(grad) < eps)
+            return {i, x};
+
+        const double x_new = x - gamma * grad; // Gradient equation (a_n+1 = a_n - η∇f(a_n))
+
+        if (std::abs(x_new - x) < eps)
+            return {i + 1, x_new};
+
+        x = x_new;
+    }
+
+    return {max_iterations, x};
+}
+
+// ====================================== T.2 c) ======================================
+void test_newton_raphson_root_find(const std::function<double(double)> &f, double x0)
+{
+    auto [iterations, x_hat] = newton_raphson_root_find(f, x0);
+    std::cout << "NR root f, x0=" << x0 << " -> it=" << iterations << " x=" << x_hat << " f(x)=" << f(x_hat) << std::endl;
+}
+
+void test_gradient_descent_minimize(const std::function<double(double)> &f, double x0)
+{
+    auto [iterations, x_hat] = gradient_descent_minimize(f, x0);
+    std::cout << "GD root f, x0=" << x0 << " -> it=" << iterations << " x=" << x_hat << " f(x)=" << f(x_hat) << std::endl;
+}
+
+void test_optimizations()
+{
+    std::cout << "Root finding tests" << std::endl;
+    auto f1 = [](double x)
+    {
+        return (x - 3.0) * (x - 3.0) - 1.0;
+    };
+    test_newton_raphson_root_find(f1, -20.0);
+    test_gradient_descent_minimize(f1, -20.0);
+}
+
 int main()
 {
     ur3e_test_fk();
+    test_optimizations();
 }
